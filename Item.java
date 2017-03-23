9a6efbf5026e369da8b51550e96ec7fb2ab6a419
Aaron Chauvette <aaronchauvette@gmail.com>
2017-03-23 16:21:53 -0400

    Updated item class
    Created methods to get the name, weight, and if the item can be picked
    up. Also added a way for items to be updated.

--- Item.java
+++ Item.java
@@ -10,16 +10,30 @@ public class Item
     // instance variables - replace the example below with your own
     private String name;
     private int weight;
-    private boolean canPickUp;
+    private boolean canTake;
 
     /**
      * Constructor for objects of class Item
      */
     public Item(String name,int weight,boolean canTake)
     {
-        String itemName = name;
-        int itemWeight = weight;
-        boolean canPickUp = canTake;
+        this.name = name;
+        this.weight = weight;
+        this.canTake = canTake;
+    }
+    public String getName(){
+        return name;
+    }
+    public int getWeight(){
+        return weight;
+    }
+    public boolean canTake(){
+        return canTake;
+    }
+    public void updateItem(String name,int weight,boolean canTake){
+        this.name = name;
+        this.weight = weight;
+        this.canTake = canTake;
     }
 
 
